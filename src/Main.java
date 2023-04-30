e203491096be360b074ad2d8cff55adad23be703
Yahir Rodríguez <126041734+yajirxz@users.noreply.github.com>
2023-04-29 23:02:19 -0600

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+package IntGrafica;
+
+import IntGrafica.IU.Principal;
+
+public class Main {
+    public static void main(String[] args) {
+        Principal ventana = new Principal("Hola Mundo AWT");
+    }
+}
